ac61fb8c6b23d15510a99287c899e902a8e93751
kirari_kiis <kirari0709@icloud.com>
2020-03-05 22:02:07 +0900

    時刻表追加

--- app.py
+++ app.py
@@ -20,24 +20,36 @@ SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(SECRET)
 
-def make_image_message():
-    messages = ImageSendMessage(
-        original_content_url="https://www.shimay.uno/nekoguruma/wp-content/uploads/sites/2/2018/03/20171124_194201-508x339.jpg",
-        preview_image_url="https://www.shimay.uno/nekoguruma/wp-content/uploads/sites/2/2018/03/20171124_194201-508x339.jpg"
-    )
-    return messages
 
-def make_button_template():
+def kiis_button():
     message_template = TemplateSendMessage(
         alt_text="webサイト",
         template=ButtonsTemplate(
             text="九州情報大学のwebサイトです",
             title="九州情報大学",
             image_size="cover",
-            thumbnail_image_url="https://www.kiis.ac.jp/wp-content/themes/kiis/img/logo02.png",
+            thumbnail_image_url="https://www.kiis.ac.jp/wp-content/themes/kiis/img/logo.png",
             actions=[
                 URIAction(
                     uri="https://www.kiis.ac.jp/",
+                    label="push!"
+                )
+            ]
+        )
+    )
+    return message_template
+
+def bus_button():
+    message_template = TemplateSendMessage(
+        alt_text="スクールバスの時刻表",
+        template=ButtonsTemplate(
+            text="九州情報大学バスの時刻表です",
+            title="バスの時刻表",
+            image_size="cover",
+            thumbnail_image_url="https://www.kiis.ac.jp/wp-content/uploads/2018/03/img_information_bus_01_02.jpg",
+            actions=[
+                URIAction(
+                    uri="https://www.kiis.ac.jp/information/bus/",
                     label="push!"
                 )
             ]
@@ -72,8 +84,13 @@ def callback():
 @handler.add(MessageEvent, message=(TextMessage))
 def handle_image_message(event):
     text = event.message.text
-    if text in ['サイト','web','さいと',"site"]:
-        messages = make_button_template()
+    if text in ['サイト','web','さいと','site','kiis']:
+        messages = kiis_button()
+        line_bot_api.reply_message(
+        event.reply_token,
+        messages)
+    elif text in ['バス','ばす','buss']:
+        messages = bus_button()
         line_bot_api.reply_message(
         event.reply_token,
         messages)
